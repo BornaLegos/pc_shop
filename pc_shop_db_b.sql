5cf3c8d75722553ddd528321eb591c3be983855b
BornaLegos <38207274+BornaLegos@users.noreply.github.com>
2018-11-26 17:00:18 +0100

    Updated database model and picture
    -Added product category
    -Added group of products (bill)

--- pc_shop_db_b.sql
+++ pc_shop_db_b.sql
@@ -24,24 +24,37 @@ location_id int not null,
 foreign key (location_id) REFERENCES location (id)
 );
 
+create table category(
+id int not null primary key auto_increment,
+name varchar(50) not null
+);
+
 create table product(
 id int not null primary key auto_increment,
 name varchar(50) not null,
 quantity int not null,
-price decimal(18,2)
+price decimal(18,2),
+category_id int not null,
+foreign key (category_id) REFERENCES category (id)
 );
 
-create table sold_product(
+create table bill(
 id int not null primary key auto_increment,
-product_id int not null,
 employee_id int not null,
 location_id int not null,
 company_id int not null,
 datetime_sold datetime,
-foreign key (product_id) REFERENCES product (id),
 foreign key (employee_id) REFERENCES employee (id),
 foreign key (location_id) REFERENCES location (id),
 foreign key (company_id) REFERENCES company (id)
+);
+
+create table sold_product(
+id int not null primary key auto_increment,
+bill_id int not null,
+product_id int not null,
+foreign key (bill_id) REFERENCES bill(id),
+foreign key (product_id) REFERENCES product(id)
 );
 
 #INSERT
@@ -56,15 +69,24 @@ insert into employee (id,first_name,last_name,oib,location_id) values
 (null,'John','Doe',23456789012,2),
 (null,'Jane','Doe',34567890123,1);
 
-insert into product (id,name,quantity,price) values
-(null,'Keyboard Z200',20,150.00),
-(null,'Mouse X300',30,300.00),
-(null,'Monitor S5000',10,1500.00),
-(null,'Headphones S5000',50,200.00);
+insert into category (id,name) values
+(null,'Laptops'),
+(null,'Computer Peripherals');
 
-insert into sold_product (id,product_id,employee_id,location_id,company_id,datetime_sold) values
-(null,3,1,2,1,'2018-11-24 16:24:30'),
-(null,4,1,2,1,'2018-11-24 16:24:30'),
-(null,2,1,2,1,'2018-11-24 16:24:30'),
-(null,1,2,1,1,'2018-11-24 18:20:40'),
-(null,2,2,1,1,'2018-11-24 18:20:40');
+insert into product (id,name,quantity,price,category_id) values
+(null,'Keyboard Z200',20,150.00,2),
+(null,'Mouse X300',30,300.00,2),
+(null,'Monitor S5000',10,1500.00,2),
+(null,'Headphones S5000',50,200.00,2),
+(null,'Laptop Q7200',5,4500.00,1);
+
+insert into bill (id,employee_id,location_id,company_id,datetime_sold) values
+(null,1,2,1,'2018-11-24 16:24:30'),
+(null,2,1,1,'2018-11-24 18:20:40');
+
+insert into sold_product (id,bill_id,product_id) values
+(null,1,1),
+(null,1,4),
+(null,2,2),
+(null,2,3),
+(null,2,5);
